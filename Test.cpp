827b8f2e6c13a931bdb635e074938daec864c654
rrph2b <102247471+rrph2b@users.noreply.github.com>
2022-03-25 21:25:09 +0100

    Add files via upload
    Lab Excercise 003

---
+++ Test.cpp
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////////
+//******************************-LAB ACTIVITY-*********************************//
+//                                                                             //
+//   NAME: IVY COBACHA        ITE6102 COMPROG 1                                //
+//                                                                             //
+//   1. Write a program that displays “Hello” in one line,                     //     
+//      and “World” in another line using exactly one(1) for                   //
+//      the "Hello" and exactly five(5)COUT statements the "World"             //   
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello";
+    cout << "World"; 
+    cout << "World";
+    cout << "World";
+    cout << "World";
+    cout << "World";
+
+return 0;
+}
